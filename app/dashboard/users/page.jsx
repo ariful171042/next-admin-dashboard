f1450f0354269f4b17abd5f90ac6bceb201a2f9f
ariful171042 <arif171042@gmail.com>
2023-11-13 19:08:37 +0600

    connect mongodb and create model, fetch users

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,11 +1,15 @@
 import styles from "../../ui/dashboard/users/users.module.css";
 import Paginations from "../../ui/dashboard/paginations/Pagination.jsx";
-
 import Search from "../../ui/dashboard/search/Search.jsx";
 import Link from "next/link";
 import Image from "next/image.js";
+import { fetchUsers } from "../../lib/data";
 
-const UserPage = () => {
+const UserPage = async () => {
+  const users = await fetchUsers();
+
+  console.log(users);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -14,6 +18,7 @@ const UserPage = () => {
           <button className={styles.addBtton}>Add New</button>
         </Link>
       </div>
+
       <table className={styles.table}>
         <thead>
           <tr>
@@ -25,39 +30,43 @@ const UserPage = () => {
             <td>Action</td>
           </tr>
         </thead>
+
         <tbody>
-          <tr>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src={"/noavatar.png"}
-                  alt=" "
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                Ariful Islam
-              </div>
-            </td>
-            <td>arif@gmail.com</td>
-            <td>10/12/2003</td>
-            <td>Admin</td>
-            <td>active</td>
-            <td>
-              <div className={styles.buttons}>
-                <Link href={"/dashboard/users/23232"}>
-                  <button className={`${styles.button} ${styles.view}`}>
-                    View
+          {users?.map((user) => (
+            <tr>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src={user.img || "/noavatar.png"}
+                    alt=" "
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.createdAt}</td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "active" : "passive"}</td>
+              <td>
+                <div className={styles.buttons}>
+                  <Link href={`/dashboard/users/${user.id}`}>
+                    <button className={`${styles.button} ${styles.view}`}>
+                      View
+                    </button>
+                  </Link>
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
                   </button>
-                </Link>
-                <button className={`${styles.button} ${styles.delete}`}>
-                  Delete
-                </button>
-              </div>
-            </td>
-          </tr>
+                </div>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
+
       <Paginations />
     </div>
   );
