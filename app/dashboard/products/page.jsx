47c26af78b88e5992da5bcd6615ab45e8f67c225
ariful171042 <arif171042@gmail.com>
2023-11-13 15:39:13 +0600

    user, products, single user, single products page done

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,51 +1,51 @@
-import styles from "../../ui/dashboard/users/users.module.css";
-import Paginations from "../../ui/dashboard/paginations/Pagination.jsx";
-
-import Search from "../../ui/dashboard/search/Search.jsx";
+import Image from "next/image";
+import styles from "../../ui/dashboard/products/products.module.css";
 import Link from "next/link";
-import Image from "next/image.js";
+import Paginations from "../../ui/dashboard/paginations/Pagination.jsx";
+import Search from "../../ui/dashboard/search/Search";
 
-const UserPage = () => {
+const Products = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <Search placeholder={"Search for user..."} />
-        <Link href={"/dashboard/users/add"}>
+        <Link href={"/dashboard/products/add"}>
           <button className={styles.addBtton}>Add New</button>
         </Link>
       </div>
       <table className={styles.table}>
         <thead>
           <tr>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Created At</td>
-            <td>Role</td>
-            <td>Status</td>
+            <td>Title</td>
+            <td>Drescription</td>
+            <td>price</td>
+            <td>stock</td>
             <td>Action</td>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>
-              <div className={styles.user}>
+              <div className={styles.product}>
                 <Image
                   src={"/noavatar.png"}
                   alt=" "
                   width={40}
                   height={40}
-                  className={styles.userImage}
+                  className={styles.productImage}
                 />
-                Ariful Islam
+                IPhone
               </div>
             </td>
-            <td>arif@gmail.com</td>
-            <td>10/12/2003</td>
-            <td>Admin</td>
-            <td>active</td>
+            <td>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea,
+              unde!
+            </td>
+            <td>$250.00</td>
+            <td>40</td>
             <td>
               <div className={styles.buttons}>
-                <Link href={"/dashboard/users/23232"}>
+                <Link href={"/"}>
                   <button className={`${styles.button} ${styles.view}`}>
                     View
                   </button>
@@ -63,4 +63,4 @@ const UserPage = () => {
   );
 };
 
-export default UserPage;
+export default Products;
